0ecc38b4a732401ef3eef8cf2e805c9106a1de5b
kuhn-moni <kuhn.moonay@gmail.com>
2023-07-28 16:50:44 +0200

    firebase installed

---
+++ react_TS_factbook/src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDto3rsKvOb7Ei_A__mP2c7CDEIuB9IwOc",
+  authDomain: "react-typescript-factbook.firebaseapp.com",
+  projectId: "react-typescript-factbook",
+  storageBucket: "react-typescript-factbook.appspot.com",
+  messagingSenderId: "885217570880",
+  appId: "1:885217570880:web:b938dd76592979aa3e2fa5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
