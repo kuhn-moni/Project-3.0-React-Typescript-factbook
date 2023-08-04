3864527893f840b14aa752a6c66c6ee0f8a75f8f
kuhn-moni <kuhn.moonay@gmail.com>
2023-08-04 11:38:39 +0200

    About text spacing

--- react_TS_factbook/src/views/About.tsx
+++ react_TS_factbook/src/views/About.tsx
@@ -1,15 +1,14 @@
 function About() {
   return (
-    <div style={{ padding: "20px" }}>
+    <div style={{ padding: "25px" }}>
       <h1>About</h1>
-      <br />
+      <hr />
       <p>
         Welcome to our website, a dynamic platform dedicated to fostering
         discourse and providing valuable information about state actors in the
         Southeast Asian region, with a particular focus on the South China Sea
         dispute.
       </p>
-      <br />
       <p>
         As a think tank initiative, our primary goal is to promote
         understanding, dialogue, and informed discussions surrounding this
@@ -20,7 +19,6 @@ function About() {
         among nations, the region has become a focal point of international
         relations.
       </p>
-      <br />
       <p>
         Our website serves as a hub for individuals, academics, policymakers,
         and anyone interested in gaining a comprehensive understanding of the
@@ -30,7 +28,6 @@ function About() {
         news articles, we strive to shed light on the historical, legal,
         economic, and geopolitical aspects of the dispute.
       </p>{" "}
-      <br />
       <p>
         Our team of experts and researchers work diligently to present accurate
         and up-to-date information, ensuring that visitors can access reliable
@@ -40,21 +37,26 @@ function About() {
         individuals can share their perspectives, engage in respectful debates,
         and learn from one another's insights. We believe that fostering
         dialogue is essential for promoting understanding and finding potential
-        solutions to complex issues. Furthermore, we regularly organize
-        webinars, conferences, and workshops, bringing together experts,
-        policymakers, and stakeholders to discuss the South China Sea dispute
-        and related topics. These events provide a platform for diverse voices
-        to be heard, facilitating constructive conversations and encouraging
-        collaboration. By promoting discourse and providing reliable
-        information, our think tank initiative aims to contribute to a more
-        nuanced understanding of the South China Sea dispute. We believe that
-        informed discussions and increased awareness can pave the way for
-        peaceful resolutions, regional stability, and cooperation among the
-        involved state actors. We invite you to explore our website, engage in
-        meaningful conversations, and join us in our mission to promote
-        discourse and understanding regarding state actors in the Southeast
-        Asian region and the South China Sea dispute. Together, we can work
-        towards a more informed and peaceful future.
+        solutions to complex issues.
+      </p>
+      <p>
+        Furthermore, we regularly organize webinars, conferences, and workshops,
+        bringing together experts, policymakers, and stakeholders to discuss the
+        South China Sea dispute and related topics. These events provide a
+        platform for diverse voices to be heard, facilitating constructive
+        conversations and encouraging collaboration. By promoting discourse and
+        providing reliable information, our think tank initiative aims to
+        contribute to a more nuanced understanding of the South China Sea
+        dispute.
+      </p>
+      <p>
+        We believe that informed discussions and increased awareness can pave
+        the way for peaceful resolutions, regional stability, and cooperation
+        among the involved state actors. We invite you to explore our website,
+        engage in meaningful conversations, and join us in our mission to
+        promote discourse and understanding regarding state actors in the
+        Southeast Asian region and the South China Sea dispute. Together, we can
+        work towards a more informed and peaceful future.
       </p>
     </div>
   );
